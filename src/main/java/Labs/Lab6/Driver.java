c54357b0340c23890c95c0b030c2c3728e095ba5
ColinCPittman <colincpittman@gmail.com>
2023-10-01 12:47:34 -0400

    submitting assignment 4 and lab 6
    Signed-off-by: ColinCPittman <colincpittman@gmail.com>

--- src/main/java/Labs/Lab6/Driver.java
+++ src/main/java/Labs/Lab6/Driver.java
@@ -1,10 +1,15 @@
 package Labs.Lab6;
 
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
         FibFormula goofy = new FibFormula();
         FibIteration goober = new FibIteration();
-        System.out.println(goofy.calculate_fib(5));
-        System.out.println(goober.calculate_fib(5));
+        System.out.print("Please enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(goofy.calculate_fib(num));
+        System.out.println(goober.calculate_fib(num));
     }
 }
